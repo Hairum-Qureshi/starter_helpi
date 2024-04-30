e55fb6ac0da83032d723baaae57ea3523f8de9ef
Hairum Qureshi <hqureshi@udel.edu>
2024-04-30 15:25:09 -0400

    resolved formatting errors

--- src/basicSlider.tsx
+++ src/basicSlider.tsx
@@ -1,23 +1,16 @@
-import React, {useState} from 'react';
-function basicSlider(
-    const basicSlider: React.FC = () => {
-        const [value, setSlideVal] = useState<number>(11);
-    
-        const adjust = (event: React.ChangeEvent<HTMLInputElement>) => {
-            setSlideVal(parseInt(event.target.value, 10));
-        };
-        return(
-            <div>
-                <input
-                type="range"
-                min={1}
-                max={10}
-                value={value}
-                onChange={handleChange}
-                />
-                <p>Value: {value} </p>
-            </div>
-        );
-    };
-)
+import React, { useState } from "react";
+function BasicSlider() {
+	const [value, setSlideVal] = useState<number>(11);
+
+	const adjust = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSlideVal(parseInt(event.target.value, 10));
+	};
+
+	return (
+		<div>
+			<input type="range" min={1} max={10} value={value} onChange={adjust} />
+			<p>Value: {value} </p>
+		</div>
+	);
+}
 export default BasicSlider;
