7b5471bff74db9077941c7e788574e98936fe8ab
Hairum Qureshi <hqureshi@udel.edu>
2024-05-12 23:29:56 -0400

    implemented logic to store user selections to localstorage and allow the user to go back and review previous selections as well as prevent users from moving ahead when they haven't answered the current question

--- src/BasicQuestionsComponents/Basic.tsx
+++ src/BasicQuestionsComponents/Basic.tsx
@@ -3,6 +3,8 @@ import basic_css from "../CSS/basic.module.css";
 import { useEffect, useState } from "react";
 import MultipleChoiceQuestion from "./MultipleChoiceQuestion";
 import RangeQuestion from "./RangeQuestion";
+import ProgressBar from "../ProgressBar";
+import { Answer } from "../detailed";
 
 export interface Option {
 	text: string;
@@ -14,6 +16,12 @@ export default function Basic() {
 	const [currentQuestionOptions, setCurrentQuestionOptions] = useState<
 		Option[]
 	>(basic_questions[currentIndex].options as Option[]);
+	const [choice, setChoice] = useState<string>();
+	const [answeredQuestions, setAnsweredQuestions] = useState<Answer[]>(
+		JSON.parse(localStorage.getItem("answered_questions_basic") || "[]")
+	);
+	const [modalVisibility, setModalVisibility] = useState(false);
+	const [showConfetti, setShowConfetti] = useState(false);
 
 	useEffect(() => {
 		setCurrentQuestionOptions(
@@ -21,113 +29,90 @@ export default function Basic() {
 		);
 	}, [currentIndex]);
 
+	function addChoice(selection: string) {
+		setChoice(selection);
+	}
+
+	function updateModalVisibility() {
+		setModalVisibility(!modalVisibility);
+		setShowConfetti(false);
+	}
+
+	function saveAnswers(choice: string, question_num: number, question: string) {
+		const updatedAnswers = answeredQuestions.map(answer =>
+			answer.questionNo === question_num ? { ...answer, choice } : answer
+		);
+
+		setAnsweredQuestions(
+			updatedAnswers.some(answer => answer.questionNo === question_num)
+				? updatedAnswers
+				: [...answeredQuestions, { question, questionNo: question_num, choice }]
+		);
+	}
+
 	return (
-		<div className={basic_css.quizContainer}>
-			<div className={basic_css.questionContainer}>
-				<h3>
-					({currentIndex + 1}/{basic_questions.length}){" "}
-					{basic_questions[currentIndex].question}
-				</h3>
+		<>
+			<div className={basic_css.quizContainer}>
+				<div className={basic_css.questionContainer}>
+					<h3>
+						({currentIndex + 1}/{basic_questions.length}){" "}
+						{basic_questions[currentIndex].question}
+					</h3>
+				</div>
+				<div className={basic_css.optionsContainer}>
+					{basic_questions[currentIndex].type === "multipleChoice" ? (
+						<MultipleChoiceQuestion
+							currentQuestionOptions={currentQuestionOptions}
+							currentIndex={currentIndex}
+							addChoice={addChoice}
+							answeredQuestions={answeredQuestions}
+							saveAnswers={saveAnswers}
+						/>
+					) : (
+						<RangeQuestion />
+					)}
+				</div>
+				<div className={basic_css.containerFooter}>
+					<div className={basic_css.progressBarContainer}>
+						<ProgressBar
+							currentIndex={currentIndex}
+							totalQuestions={basic_questions.length}
+						/>
+					</div>
+					<button
+						disabled={currentIndex === 0}
+						onClick={() => {
+							setCurrentIndex(index => (index -= 1 % basic_questions.length));
+							setChoice(
+								answeredQuestions[currentIndex] &&
+									answeredQuestions[currentIndex - 1].choice
+							);
+						}}
+					>
+						{currentIndex === 0 ? "END" : "PREV."}
+					</button>
+					<button
+						disabled={!choice || choice.length > 500}
+						onClick={() => {
+							if (currentIndex === basic_questions.length - 1) {
+								setModalVisibility(!modalVisibility);
+								setShowConfetti(true);
+
+								setTimeout(() => {
+									setShowConfetti(false);
+								}, 8000);
+							} else {
+								setCurrentIndex(index => index + 1);
+								setChoice(answeredQuestions[currentIndex + 1]?.choice || "");
+							}
+						}}
+					>
+						{currentIndex === basic_questions.length - 1
+							? "SUBMIT RESPONSES"
+							: "NEXT"}
+					</button>
+				</div>
 			</div>
-			<div className={basic_css.optionsContainer}>
-				{basic_questions[currentIndex].type === "multipleChoice" ? (
-					<MultipleChoiceQuestion
-						currentQuestionOptions={currentQuestionOptions}
-					/>
-				) : (
-					<RangeQuestion />
-				)}
-			</div>
-			<div className={basic_css.containerFooter}>
-				<button
-					disabled={currentIndex === 0}
-					onClick={() =>
-						setCurrentIndex(
-							currentIndex => (currentIndex - 1) % basic_questions.length
-						)
-					}
-				>
-					Previous
-				</button>
-				<button
-					disabled={currentIndex === basic_questions.length - 1}
-					onClick={() =>
-						setCurrentIndex(
-							currentIndex => (currentIndex + 1) % basic_questions.length
-						)
-					}
-				>
-					Next
-				</button>
-			</div>
-		</div>
+		</>
 	);
 }
-
-// import { useState } from "react";
-// import questions from "../basicQuestions.json";
-// // import QuestionFormatProps from "../interfaces/questionFormat";
-// import { BasicOptions } from "../interfaces/basicOption";
-// import basic_css from "./basic.module.css";
-// import ProgressBar from "../ProgressBar";
-// import MultipleChoiceQuestion from "./MultipleChoiceQuestion";
-// // import RangeComponent from "./RangeComponent";
-
-// function Basic() {
-// 	const [currentIndex, setCurrentIndex] = useState(0);
-
-// 	const handleNext = () => {
-// 		setCurrentIndex(prevIndex => prevIndex + 1);
-// 	};
-
-// 	const handlePrev = () => {
-// 		setCurrentIndex(prevIndex => prevIndex - 1);
-// 	};
-
-// 	return (
-// 		<div>
-// 			<ProgressBar
-// 				currentIndex={currentIndex}
-// 				totalQuestions={questions.length}
-// 			/>
-// 			<div className={basic_css.quizContainer}>
-// 				<div key={currentIndex}>
-// 					<div className={basic_css.questionContainer}>
-// 						<h3>
-// 							({questions[currentIndex]?.question_number}/{questions.length})
-// 							&nbsp;
-// 							{questions[currentIndex]?.question}
-// 						</h3>
-// 					</div>
-// 					{questions[currentIndex]?.type === "multipleChoice" ? (
-// 						<MultipleChoiceQuestion
-// 							options={questions[currentIndex]?.options as BasicOptions[]}
-// 							question={questions[currentIndex]?.question}
-// 							type={questions[currentIndex]?.type}
-// 							question_number={questions[currentIndex]?.question_number}
-// 						/>
-// 					) : (
-// 						<h1>Range component here</h1>
-// 						// <RangeComponent
-// 						// 	options={questions[currentIndex]?.options as BasicOptions[]}
-// 						// 	question={questions[currentIndex]?.question}
-// 						// 	type={questions[currentIndex]?.type}
-// 						// 	question_number={questions[currentIndex]?.question_number}
-// 						// />
-// 					)}
-// 					<button onClick={handlePrev} disabled={currentIndex === 0}>
-// 						Previous
-// 					</button>
-// 					<button
-// 						onClick={handleNext}
-// 						disabled={currentIndex === questions.length - 1}
-// 					>
-// 						Next
-// 					</button>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// }
-
-// export default Basic;
--- src/BasicQuestionsComponents/MultipleChoiceQuestion.tsx
+++ src/BasicQuestionsComponents/MultipleChoiceQuestion.tsx
@@ -1,21 +1,67 @@
+import { useEffect } from "react";
 import { Option } from "./Basic";
+import basic_questions from "../JSON_files/basicQuestions.json";
+import { Answer } from "../detailed";
 
 interface Props {
 	currentQuestionOptions: Option[];
+	currentIndex: number;
+	addChoice: (selection: string) => void;
+	answeredQuestions: Answer[];
+	saveAnswers: (choice: string, question_num: number, question: string) => void;
 }
 
 export default function MultipleChoiceQuestion({
-	currentQuestionOptions
+	currentQuestionOptions,
+	currentIndex,
+	addChoice,
+	answeredQuestions,
+	saveAnswers
 }: Props) {
+	useEffect(() => {
+		localStorage.setItem("current_question_basic", currentIndex.toString());
+		localStorage.setItem(
+			"answered_questions_basic",
+			JSON.stringify(answeredQuestions)
+		);
+	}, [currentIndex, answeredQuestions]);
+
 	return (
 		<>
-			{currentQuestionOptions.map((option: Option) => {
+			{currentQuestionOptions.map((option: Option, index: number) => {
 				return (
-					<button key={option.text}>
+					<button
+						key={index}
+						onClick={() => {
+							addChoice(option.text);
+							saveAnswers(
+								option.text,
+								basic_questions[currentIndex].question_number,
+								basic_questions[currentIndex].question
+							);
+						}}
+						style={{
+							backgroundColor: `${
+								answeredQuestions.some(
+									selectedAnswer => selectedAnswer.choice === option.text
+								)
+									? "#006BA6"
+									: "#003459"
+							}`,
+							transition: "0.25s ease",
+							border: `${
+								answeredQuestions.some(
+									selectedAnswer => selectedAnswer.choice === option.text
+								)
+									? "2px solid cyan"
+									: "none"
+							}`
+						}}
+					>
 						<span>
 							<img src={option.image} alt="Question choice visual" />
 						</span>
-						<span>{option.text}</span>
+						{option.text}
 					</button>
 				);
 			})}
