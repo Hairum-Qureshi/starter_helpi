7ad0c0ba5bfbd7b5d91498b8ceecb90c7de4bfc2
Hairum Qureshi <hqureshi@udel.edu>
2024-05-14 10:36:21 -0400

    added feature to display 'resume [quiz type]' text to the two buttons if the user has started either buttons

--- src/basicDetailedButton.tsx
+++ src/basicDetailedButton.tsx
@@ -4,18 +4,28 @@ import "./CSS/basicDetailedButton.css";
 function BasicDetailedButton() {
 	const navigate = useNavigate();
 
+	const basic_quiz: string | null = localStorage.getItem(
+		"answered_questions_basic"
+	);
+	const detailed_quiz: string | null =
+		localStorage.getItem("answered_questions");
+
 	return (
 		<>
 			<div className="buttonContainer">
 				<div className="basicBtnContainer">
-					<button onClick={() => navigate("/basic")}>Basic Quiz</button>
+					<button onClick={() => navigate("/basic")}>
+						{basic_quiz ? "Resume Basic Quiz" : "Start Basic Quiz"}
+					</button>
 					<p>
 						This is a 10 - 15 minute quiz aimed to gauge interests and skills to
 						provide suggestions and information on potential careers.
 					</p>
 				</div>
 				<div className="detailedBtnContainer">
-					<button onClick={() => navigate("/detailed")}>Detailed Quiz</button>
+					<button onClick={() => navigate("/detailed")}>
+						{detailed_quiz ? "Resume Detailed Quiz" : "Start Detailed Quiz"}
+					</button>
 					<p>
 						This is a 30 - 35 minute quiz aimed to understand your goals and
 						passions in order to provide potential careers.
